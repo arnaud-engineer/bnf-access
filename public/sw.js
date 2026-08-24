b826400dc9e540d652012f818b9feccba4d99e8b
arnaud <f@nanaud.fr>
2026-08-24 20:38:09 +0200

    Isolate PWA install scope

--- public/sw.js
+++ public/sw.js
@@ -1,30 +1,5 @@
-const cacheName = "bnf-access-v6";
-const appShellUrls = [
-  "./",
-  "./app.html",
-  "./index.html",
-  "./faq.html",
-  "./mentions-legales.html",
-  "./styles.css",
-  "./site.js",
-  "./pwa.js",
-  "./faq.js",
-  "./app.js",
-  "./resources.json",
-  "./bnf-network.json",
-  "./manifest.webmanifest",
-  "./bnf-access-icon.svg",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
-  "./icons/apple-touch-icon.png",
-  "./logos/arnaud-cool.svg",
-  "./logos/kofi.svg",
-];
-
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(cacheName).then((cache) => cache.addAll(appShellUrls))
-  );
+  event.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener("activate", (event) => {
@@ -32,43 +7,9 @@ self.addEventListener("activate", (event) => {
     caches
       .keys()
       .then((keys) =>
-        Promise.all(keys.filter((key) => key !== cacheName).map((key) => caches.delete(key)))
+        Promise.all(keys.filter((key) => key.startsWith("bnf-access-v")).map((key) => caches.delete(key)))
       )
+      .then(() => self.registration.unregister())
       .then(() => self.clients.claim())
   );
 });
-
-self.addEventListener("fetch", (event) => {
-  const request = event.request;
-
-  if (request.method !== "GET") {
-    return;
-  }
-
-  const requestUrl = new URL(request.url);
-  if (requestUrl.origin !== self.location.origin) {
-    return;
-  }
-
-  if (request.mode === "navigate") {
-    event.respondWith(fetch(request).catch(() => caches.match("./index.html")));
-    return;
-  }
-
-  event.respondWith(cacheFirst(request));
-});
-
-async function cacheFirst(request) {
-  const cached = await caches.match(request);
-  if (cached) {
-    return cached;
-  }
-
-  const response = await fetch(request);
-  if (response.ok) {
-    const cache = await caches.open(cacheName);
-    cache.put(request, response.clone());
-  }
-
-  return response;
-}
